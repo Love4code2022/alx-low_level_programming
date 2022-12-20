d4c3fa1dd692754e24764643bf5c39ef67137b3f
love4code2022 <professorazeez2020@gmail.com>
2022-12-20 12:05:08 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * Return: always 0 (success)
  */
 
-  int main(void)
+int main(void)
 	{
-		printf("with proper grammar, but the outcome is a piece of art,");
+		printf("with proper grammar, but the outcome is a piece of art, ");
 		return (0);
 	}
