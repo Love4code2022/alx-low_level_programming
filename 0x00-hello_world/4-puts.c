0ef81ed19b568a502e9b262f5d7178c2a0005b66
love4code2022 <professorazeez2020@gmail.com>
2022-12-20 10:08:14 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main-entry point
  * return-always zero
  */
 
 int main(void)
-
-{
+	{
 	puts("progarmming is like building a multilingual puzzle\n");
-	return(0);
-}
+	return (0);
+	}
