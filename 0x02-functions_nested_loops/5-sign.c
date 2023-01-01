59db3071abd159223f7f8319a37abf263d1c4a1e
love4code2022 <professorazeez2020@gmail.com>
2022-12-31 16:33:21 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: number whose sign is checked
+ * Return: 1 if it is greater than 1, 0 if it is 0, -1 if it is less than 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
